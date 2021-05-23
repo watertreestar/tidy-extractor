863eb9d908ec4631748a67d6c7bc0a5ff4e2b2e2
watertreestar <watertreestar@gmail.com>
2021-05-23 21:05:20 +0800

    refact exporter

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # tidy-extractor
 
-平常使用的网站内容下载，比如bilibili视频下载，拉钩教育专栏下载
+从网页上把拉勾教育专栏，csdn专栏，bilibili视频抓下来，导出markdown，pdf等多种格式
 
-# 已经实现的
+## 背景
+
+
+
+## done
 
 - [x] bilibili视频
 - [x] 拉钩教育专栏
+- [x] CSDN
+
+
+## todo
---
+++ download/exporter.py
@@ -0,0 +1,19 @@
+"""
+Different Exporter
+"""
+
+
+class BaseExporter:
+    """
+    Base Export for all exporter
+    """
+    def __init__(self):
+        pass
+
+
+class PDFExporter(BaseExporter):
+    """
+    Export content as a PDF file
+    """
+    def __init__(self):
+        pass
