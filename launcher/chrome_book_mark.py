74254f3b4dc91eb9a32cbef212fa12a9ead0a89b
watertreestar <watertreestar@gmail.com>
2021-06-20 04:59:31 +0800

    duplicate judgement

--- launcher/chrome_book_mark.py
+++ launcher/chrome_book_mark.py
@@ -2,10 +2,10 @@ import re
 
 import requests
 
-from export.msyql_export import MySQLExporter
+from export.mysql_exporter import MySQLExporter
 from google_bookmark import GoogleBookMark
 from tools.util import de_html
-from main import logger
+from tools.logger import logger
 
 PROXIES = {
     'http': 'http://127.0.0.1:1087',
@@ -21,10 +21,15 @@ def start(path):
     logger.info("Find %s link", str(len(github_links)))
     failed_links = []
     for link in github_links:
+        if mysql_exporter.exist('chrome_book_mark', ('url', link)):
+            logger.info("Exist this, skip......")
+            continue
         try:
             logger.info("Extract url [%s]", link)
             html_text = requests.get(link, proxies=PROXIES).text
             title = re.findall(r"<title.*?>(.+?)</title>", html_text)[0]
+            if len(title) > 200:
+                title = title[0:200]
             text = de_html(html_text)
             normal_link = re.sub(r"#.*", "", title)
             data = [('url', link), ('title', normal_link), ('content', text)]
